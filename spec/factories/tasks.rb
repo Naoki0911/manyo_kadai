d1e75213ceecb16842d023420aaf8cee31e4dd46
naoki ban <bluefoxxexx@gmail.com>
2023-03-16 13:33:29 +0900

    万葉課題step2提出

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :task do 
-    title { 'test_title'}
-    content { 'test_content'}
+    title { 'test_title' }
+    content { 'test_content' }
+    limit { '2023/3/20' }
   end
 end
